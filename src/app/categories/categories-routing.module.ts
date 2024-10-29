6002db7b01c375b6eee3c00b6d35dbfc311b2d97
Angel Hernandez <angelillohdez@gmail.com>
2024-10-29 13:46:35 +0100

    feat: create routing for categories module

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 const routes: Routes = [
   // { path: '', component: CategoriesComponent }
+  { path: ':idCategory', component: ProductListComponent},
+  { path: 'products/:idProduct', component: ProductCardComponent}
 ];
 
 @NgModule({
